6f7751ff7a2bd522e9f611c61fd6f0ffefcfbf93
Dream <503963720@qq.com>
2018-05-25 16:37:37 +0800

    增加用户管理模块

---
+++ use/src/main/java/com/dream/use/UseApplication.java
@@ -0,0 +1,12 @@
+package com.dream.use;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseApplication.class, args);
+	}
+}
